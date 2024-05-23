c6900aa60f52c2336c80a8d3e876878a1c47cd13
ryantoxx <nikitachihai2@gmail.com>
2024-05-23 23:22:49 +0300

    feat: implement CRUD

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models.database import db
+from flask_swagger_ui import get_swaggerui_blueprint
 from models.recipes import Recipe
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_cors import CORS
 from datetime import timedelta
+
+def authenticate(username, password):
+    users = {
+        'writer': 'writer',
+        'admin': 'admin'
+    }
+    if username in users and users[username] == password:
+        return username
 
 def create_app():
     app = Flask(__name__)
+    CORS(app) 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
     db.init_app(app)
     return app
@@ -17,6 +28,15 @@ if __name__ == '__main__':
     app.config['JWT_SECRET_KEY'] = 'secret'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=1)
     jwt = JWTManager(app)
+    
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={'Recipe    ': 'Recipe API'}
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
     
     import routes
     app.run(port=6060, debug=True)
--- routes.py
+++ routes.py
@@ -28,3 +28,116 @@ def get_token():
     else:
         return jsonify({'error': 'Username and password required'}), 400
 
+# GET REQUEST
+@app.route('/api/recipes', methods=['GET'])
+@jwt_required()
+def get_recipes():
+    try:
+        current_user = get_jwt_identity()
+
+        role = 'WRITER' if current_user == 'writer' else 'ADMIN'
+
+        if role not in ['ADMIN', 'WRITER']:
+            return jsonify({"msg": "Not allowed"}), 403
+        
+        # Retrieve query parameters for pagination
+        page = int(request.args.get('page', 1))
+        limit = int(request.args.get('limit', 10))  # Default limit to 10 if not provided
+
+        # Calculate the offset based on the page and limit
+        offset = (page - 1) * limit
+
+        # Query recipes with pagination
+        recipes = Recipe.query.offset(offset).limit(limit).all()
+        total_recipes = Recipe.query.count()
+        total_pages = (total_recipes + limit - 1) // limit
+
+        search_query = request.args.get('q', '')
+        recipes = Recipe.query.filter(Recipe.title.ilike(f'%{search_query}%')).all()
+        data = [recipe.to_dict() for recipe in recipes]
+
+        return jsonify(data), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+   
+@app.route('/api/recipes/<int:recipe_id>', methods=['GET'])
+def get_recipe_by_id(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if recipe is not None:
+        return jsonify({
+            "title": recipe.title,
+            "ingredients": recipe.ingredients,
+            "servings": recipe.servings,
+            "instructions": recipe.instructions
+        }), 200
+    else:
+        return jsonify({"error": "Recipe not found"}), 404
+
+# POST REQUEST
+@app.route('/api/recipes', methods=['POST'])
+@jwt_required()
+def create_recipe():
+    try:
+        current_user = get_jwt_identity()
+
+        role = 'WRITER' if current_user == 'writer' else 'ADMIN'
+
+        if role not in ['ADMIN', 'WRITER']:
+            return jsonify({"msg": "Not allowed"}), 403
+        
+        data = request.get_json()
+        new_recipe = Recipe(**data)
+        db.session.add(new_recipe)
+        db.session.commit()
+        return jsonify({"message": "Recipe created successfully"}), 201
+    except KeyError:
+        return jsonify({"error": "Invalid request data"}), 400
+
+# PUT
+@app.route('/api/recipes/<int:recipe_id>', methods=['PUT'])
+@jwt_required()
+def update_recipe(recipe_id):
+    try:
+        current_user = get_jwt_identity()
+
+        role = 'WRITER' if current_user == 'writer' else 'ADMIN'
+
+        if role not in ['ADMIN', 'WRITER']:
+            return jsonify({"msg": "Not allowed"}), 403
+        
+        data = request.get_json()
+        recipe = Recipe.query.get(recipe_id)
+            
+        if recipe is not None:
+            
+            for key, value in data.items():
+                setattr(recipe, key, value)
+                
+            db.session.commit()
+                
+            return jsonify({"message": "Recipe updated successfully"}), 200
+        else:
+            return jsonify({"error": "Recipe not found"}), 404
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/recipes/<int:recipe_id>', methods=['DELETE'])
+@jwt_required()
+@admin_required
+def delete_recipe(recipe_id):
+    try:
+        recipe = Recipe.query.get(recipe_id)
+        if recipe is not None:
+            db.session.delete(recipe)
+            db.session.commit()
+            return jsonify({"message": "Recipe deleted successfully"}), 200
+        else:
+            return jsonify({"error": "Recipe not found"}), 404
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+
